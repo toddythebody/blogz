75b04e52934fd3416814b78974c72f5678b2126b
Todd Sansom <todd.sansom@gmail.com>
2019-04-09 10:24:21 -0500

    salt

--- pwhash.py
+++ pwhash.py
@@ -1,9 +1,18 @@
 import hashlib
+import random
+import string
 
-def makeHash(pw):
-    return hashlib.sha256(str.encode(pw)).hexdigest()
+def makeSalt():
+    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
+
+def makeHash(pw, salt=None):
+    if not salt:
+        salt = makeSalt()
+    hash = hashlib.sha256(str.encode(pw + salt)).hexdigest()
+    return '{0},{1}'.format(hash, salt)
 
 def checkHash(pw, hash):
-    if makeHash(pw) == hash:
+    salt = hash.split(',')[1]
+    if makeHash(pw, salt) == hash:
         return True
     return False
